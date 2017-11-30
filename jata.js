c2d703000f1689d94b6e55a5e12b64f5e673b773
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2017-11-30 13:18:45 +0000

    Update jata.js

--- jata.js
+++ jata.js
@@ -5,7 +5,7 @@ if($("#mirt").attr("href")!="https://tesgfhbg.blogspot.com/" )
  {
    window.location.href="http://rawgit.com/";
  }
-  if($("#mirt").html()!= ''){
+  if($("#mirt").html()!= 'ddd'){
    window.location.href="http://rawgit.com/";
 
   }
