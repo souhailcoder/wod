f927d916bff064c8f819868036a7b13363e40766
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2017-11-30 09:37:00 +0000

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,19 +1,16 @@
 {
    
-
-if($("#fire").attr("href")!="https://slah-template.blogspot.com/" )
 if($("#fire").attr("href")!="https://tesgfhbg.blogspot.com/" )
-if($("#fire").attr("href")!="http://www.sweuddp.com" )
 
  {
-   window.location.href="http://www.swçeup.com";
+   window.location.href="http://rawgit.com/";
  }
   if($("#fire").html()!= ''){
-   window.location.href="http://www.sweup.com";
+   window.location.href="http://rawgit.com/";
 
   }
 if($("#fire").css('display')== 'none'){
-   window.location.href="http://www.sweup.com";
+   window.location.href="http://rawgit.com/";
 
   }
 
