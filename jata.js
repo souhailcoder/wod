02fcba8b2cdb8f800823b748c60ef3c0e4b8b590
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2017-11-30 20:16:43 +0000

    Update jata.js

--- jata.js
+++ jata.js
@@ -3,14 +3,8 @@
         function redirect(){
             window.location.assign('https://slash-template.blogspot.com/');
         }
-
-        function check(){
-                if($('#mirt').length === 0){
-                    redirect();
                 }
-
                 else if($("#mirt").attr("href") !== "https://slash-late.blogspot.com/"){
                     redirect();
                 }
-            }
-        }});
+        });
