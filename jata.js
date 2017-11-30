fc6ae30e9c75e91cb7fbdbe4ecfec811de1e8bdd
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2017-11-30 23:04:45 +0000

    Update jata.js

--- jata.js
+++ jata.js
@@ -1,4 +1,19 @@
-  
-    if($("#mirt").attr("href")!="https://slash-template.blogspot.com/") 
-  { 
-     window.location.href="https://www.facebook.com/souhail.talyani"; };
+{
+   
+
+if($("#mirt").attr("href")!="https://rawgit.com/")
+if($("#mirt").attr("href")!="https://slash-template.blogspot.com/")
+
+ {
+   window.location.href="https://www.blogger.com/?hl=ar&tab=jj";
+ }
+  if($("#mirt").html()!= ''){
+   window.location.href="https://www.blogger.com/?hl=ar&tab=jj";
+
+  }
+if($("#mirt").css('display')== 'none'){
+   window.location.href="https://www.blogger.com/?hl=ar&tab=jj";
+
+  }
+
+    });
