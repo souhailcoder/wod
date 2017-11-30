9d4c8707f44f45ee6616f7211379974f163b89da
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2017-11-30 23:01:57 +0000

    Update jata.js

--- jata.js
+++ jata.js
@@ -1,3 +1,4 @@
-   { 
+  
     if($("#mirt").attr("href")!="https://slash-template.blogspot.com/") 
-  { window.location.href="https://www.facebook.com/souhail.talyani"; }};
+  { 
+     window.location.href="https://www.facebook.com/souhail.talyani"; };
