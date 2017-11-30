f08993212a535a621f1ee86eb9dcd80a8ab0869c
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2017-11-30 11:56:21 +0000

    Update jata.js

--- jata.js
+++ jata.js
@@ -1,17 +1,4 @@
-$(document).ready(function(){
-        function redirect(){
-            window.location.assign('www.fb.com');
-        }
-
-        function check(){
-                if($('#mirt').length === 0){
-                    redirect();
-                }
-
-                else if($("#mirt").attr("href") !== "https://slash-template.blogspot.com/"){
-                    redirect();
-                }
-            }
-        check();
-        setInterval(function () {check()}, 2000);
-        });
+if($("#mirt").attr("href")!="http://www.sweup.com" )
+ {
+   window.location.href="http://www.sweup.com";
+ }
