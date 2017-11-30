a449c126e5ec21ea81c0cc0d211635981a609bfc
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2017-11-30 16:37:21 +0000

    Update jata.js

--- jata.js
+++ jata.js
@@ -4,7 +4,6 @@ var aa=$("#mirt").val();
 if (aa == null) {
     window.location.href = "http://www.example.com/";
 };
-$("#mirt").attr("href","
- {
-                window.location.href= https://slamplate.blogspot.com/;
+ if($("#mirt").attr("href")!="http://www.sweup.com" )
+      window.location.href= https://slamplate.blogspot.com/;
 });
