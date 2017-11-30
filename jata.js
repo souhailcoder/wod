c73575d2e6f8b205b57080ff10f5bb56031fb4f2
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2017-11-30 22:41:05 +0000

    Update jata.js

--- jata.js
+++ jata.js
@@ -1,5 +1,3 @@
-  $(document).ready(function() 
-  { 
-    if($("#mirt").attr("href")!="slash-template.blogspot.com") 
-       if($("#mirt").attr("href")!="https://slash-template.blogspot.com/") 
+   { 
+    if($("#mirt").attr("href")!="https://slash-template.blogspot.com/") 
   { window.location.href="https://www.facebook.com/souhail.talyani"; }});
