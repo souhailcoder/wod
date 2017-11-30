003690c870c0ab4bcfdfcaf5688e8a630954b088
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2017-11-30 14:00:43 +0000

    Update jata.js

--- jata.js
+++ jata.js
@@ -1,5 +1,6 @@
-if($("#mirt").attr("href")!="https://slash-template.blogspot.com/" )
-
+{
+ if($("#mirt").attr("href")!="https://slash-template.blogspot.com/" )
+ }
  {
    window.location.href="http://rawgit.com/";
  }
