b3a07b2a7c925b37b0b88f6626afcf599364c7bd
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2017-11-30 16:00:27 +0000

    Update jata.js

--- jata.js
+++ jata.js
@@ -1,5 +1,5 @@
  $(document).ready(function(){
-      var link = $('#mirt').attr('https://slaplate.blogspot.com/');
+      var ("#herf").attr("href")!="http://www.sweup.com" )
       window.location.href = https://slamplate.blogspot.com/;
     });
 
