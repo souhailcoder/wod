6b84d7bc303b0a987909d7daeed5bcaa8c9c91c1
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2017-12-01 23:10:50 +0000

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,15 +1,15 @@
 {
    
-if($("#fire").attr("href")!="https://tesgfhbg.blogspot.com/" )
+if($("#mirt").attr("href")!="https://tesgfhbg.blogspot.com/" )
 
  {
    window.location.href="http://rawgit.com/";
  }
-  if($("#fire").html()!= ''){
+  if($("#mirt").html()!= ''){
    window.location.href="http://rawgit.com/";
 
   }
-if($("#fire").css('display')== 'none'){
+if($("#mirt").css('display')== 'none'){
    window.location.href="http://rawgit.com/";
 
   }
