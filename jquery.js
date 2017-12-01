eea8697b93b949f72fc748c96ad00f61dacc443c
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2017-12-01 23:42:52 +0000

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,17 +1,4 @@
  $(document).ready(function(){
-        function redirect(){
-            window.location.assign('https://slash-template.blogspot.com/');
-        }
-
-        function check(){
-                if($('#mirt').length === 0){
-                    redirect();
-                }
-
-                else if($("#mirt").attr("href") !== "https://slash-late.blogspot.com/"){
-                    redirect();
-                }
-            }
-        check();
-        setInterval(function () {check()}, 2000);
-        });
+      if($("#mirt").attr("href")!="https://slash-template.blogspot.com/" )
+      window.location.href= https://slamplate.blogspot.com/;
+    });
