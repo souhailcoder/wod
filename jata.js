b008a77a552cc8adf35ca08948453cee9d63684d
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2017-12-03 21:04:01 +0000

    Update jata.js

--- jata.js
+++ jata.js
@@ -2,7 +2,7 @@ $(document).ready(function()
 {
    
 
-if($("#blogurl").attr("href")!="" )
+if($("#blogurl").attr("href")!="https://slash-template.blogspot.com/" )
  {
    window.location.href="http://www.sweup.com";
  }
