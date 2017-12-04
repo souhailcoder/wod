4f0e9a63108c601f8841c3497c52ebaa8b680617
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2017-12-04 11:37:43 +0000

    Update jata.js

--- jata.js
+++ jata.js
@@ -2,7 +2,7 @@ $(document).ready(function()
 {
    
 
-if($("#blogurl").attr("href")!="https://slash-template.blogspot.com/" )
+if($("#blogurl").attr("blogID")!="571211818552536631" )
    if($("#blogurl").attr("href")!="https://tesgfhbg.blogspot.com/" )
 
  {
