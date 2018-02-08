789dd927467efe21ca4b443bc26ae1712ffcef8c
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2018-02-08 14:04:48 +0000

    Update jata.js

--- jata.js
+++ jata.js
@@ -2,9 +2,7 @@ $(document).ready(function()
 {
    
 
-if($("#blogID").attr("blogID")!="" )
-   if($("#blogID").attr("blogID")!="2435297633935014706" )
-   if($("#blogID").attr("blogID")!="3766654720502656154" ) 
+
 
  {
    window.location.href="http://www.sweup.com";
