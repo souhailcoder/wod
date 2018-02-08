2d6234a6859d29ee455f5e3b20ca6b250d7da194
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2018-02-08 14:09:54 +0000

    Update jata.js

--- jata.js
+++ jata.js
@@ -1,6 +1,6 @@
 $(document).ready(function()
 {
-   if($("#blogID").attr("blogID")!="571211818552536631" )
+   if($("#blogID").attr("blogID")!="57121181855253663" )
    if($("#blogID").attr("blogID")!="2435297633935014706" )
    if($("#blogID").attr("blogID")!="3766654720502656154" ) 
  {
