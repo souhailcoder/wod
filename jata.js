6ae1b5f46a006bb94385b744d3ccca3bb0422851
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2018-02-09 15:35:11 +0000

    Update jata.js

--- jata.js
+++ jata.js
@@ -3,6 +3,6 @@ $(document).ready(function()
    if($("#blogID").attr("blogID")!="571211818552536631" )
    if($("#blogID").attr("blogID")!="5073174128705643314" )
  {
-   window.location.href="http://www.sweup.com";
+   window.location.href="https://picalica.com/u/SOUHAILLEKHOUAJA/";
  }
     });
