db88df64f581a0d6397ba4dfc813a0f3e9755023
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2018-02-18 17:10:01 +0000

    Update soso.js

--- soso.js
+++ soso.js
@@ -1,6 +1,6 @@
 $(document).ready(function()
 {
-   if($("#blogID").attr("blogID")!="https://spacio-demo.blogspot.com/" )
+   if($("#blogID").attr("blogID")!="3130731078515920927" )
  {
    window.location.href="https://picalica.com/u/SOUHAILLEKHOUAJA/";
  }
