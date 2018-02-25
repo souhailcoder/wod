9cac4510671f0a0ab536d6eab397a7b67d23d72a
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2018-02-25 11:09:33 +0000

    Update soso.js

--- soso.js
+++ soso.js
@@ -1,6 +1,7 @@
 $(document).ready(function()
 {
    if($("#blogID").attr("blogID")!="3130731078515920927" )
+   if($("#blogID").attr("blogID")!="7601418572176364349" )
  {
    window.location.href="https://picalica.com/u/SOUHAILLEKHOUAJA/";
  }
