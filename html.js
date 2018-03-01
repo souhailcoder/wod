4db34e21d389ad14ce2208181a52b6ddc3d5b68a
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2018-03-01 12:33:01 +0000

    Update html.js

--- html.js
+++ html.js
@@ -2,6 +2,7 @@ $(document).ready(function()
 {
    if($("#blogID").attr("blogID")!="3130731078515920927" )
    if($("#blogID").attr("blogID")!="7601418572176364349" )
+   if($("#blogID").attr("blogID")!="5291694035456160539" )
  {
    window.location.href="https://picalica.com/u/SOUHAILLEKHOUAJA/";
  }
