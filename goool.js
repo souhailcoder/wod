2d083dae4cd5e9822a9faf44a15a9158e744a7d3
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2018-08-26 21:11:40 +0100

    Update goool.js

--- goool.js
+++ goool.js
@@ -1,6 +1,6 @@
 $(document).ready(function()
 {
-   if($("#blogID").attr("blogID")!="" )
+   if($("#blogID").attr("blogID")!="1319453562109954732" )
  {
    window.location.href="https://www.facebook.com/souhail.talyani";
  }
