c86de1e6a56074d3511672097083fe8c8d42e89a
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2019-10-19 18:06:14 +0100

    Update alph.js

--- alph.js
+++ alph.js
@@ -4,17 +4,19 @@ var allahref=document.getElementsByTagName("a");
 for(var c=0;c<allahref.length;c++)
 {
 if(allahref[c].hostname!="www.yalla-live.tv" && allahref[c].hostname!="")
-if(allahref[c].hostname!="web.facebook.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.facebook.com" && allahref[c].hostname!="")
 if(allahref[c].hostname!="www.youtube.com" && allahref[c].hostname!="")
 if(allahref[c].hostname!="play.google.com" && allahref[c].hostname!="")
 if(allahref[c].hostname!="www.linkedin.com" && allahref[c].hostname!="")
-if(allahref[c].hostname!="www.t.me" && allahref[c].hostname!="")
-if(allahref[c].hostname!="www.twitter.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="t.me" && allahref[c].hostname!="")
+if(allahref[c].hostname!="twitter.com" && allahref[c].hostname!="")
 if(allahref[c].hostname!="1.bp.blogspot.com" && allahref[c].hostname!="")
 if(allahref[c].hostname!="2.bp.blogspot.com" && allahref[c].hostname!="")
 if(allahref[c].hostname!="3.bp.blogspot.com" && allahref[c].hostname!="")
 if(allahref[c].hostname!="4.bp.blogspot.com" && allahref[c].hostname!="")
 if(allahref[c].hostname!="www.blogger.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.instagram.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="chat.whatsapp.com" && allahref[c].hostname!="")
 if(allahref[c].hostname!="www.instagram.com" && allahref[c].hostname!="")
 {
 allahref[c].href="https://www.yalla-live.tv/p/wait.html??&&url=_"+allahref[c].href;
