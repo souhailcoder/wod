6cee7c4cd073cf78dc18175f20333e0f695e9df7
souhailcoder <34027030+souhailcoder@users.noreply.github.com>
2021-06-10 19:25:30 +0100

    Create lstm.js

---
+++ lstm.js
@@ -0,0 +1,79 @@
+
+
+var _0x41d3 = [ "empty", "<a name='", "'></a>", "before", "<li><a href='#", "'>", "text", "</a></li>", "h2 , h3", "is", "<ul></ul>", "append", "#tocList", "appendTo", "each", ".amp-content h2, .amp-content h3", "ready", "a3lan-title", "getElementById", "innerHTML", "getElementsByClassName", "remove", "a3lan-first", "a3lan-Center", "a3lan-End", "a3lan-lates", "href", "attr", "a.blog-pager-older-link", "show", ".loadMorePosts", "click", "hide", ".blog-posts", "find", ".status-msg-wrap", "children", "html", ".blog-pager-older-link", ".noMorePosts", ".loadMore > #loader", "ajax", "preventDefault", "on", ".loadMorePosts a", ".poweredby", "https://www.almosamm.com", "locaition" ];
+
+$(document)[_0x41d3[16]](function() {
+    $(tocList)[_0x41d3[0]]();
+    var a = null, b = null, c = 0;
+    $(_0x41d3[15])[_0x41d3[14]](function() {
+        var d = _0x41d3[1] + c + _0x41d3[2];
+        $(this)[_0x41d3[3]](d);
+        var e = _0x41d3[4] + c + _0x41d3[5] + $(this)[_0x41d3[6]]() + _0x41d3[7];
+        $(this)[_0x41d3[9]](_0x41d3[8]) ? (b = $(_0x41d3[10]), (a = $(e))[_0x41d3[11]](b), 
+        a[_0x41d3[13]](_0x41d3[12])) : b[_0x41d3[11]](e), c++;
+    });
+});
+
+var _0xf104 = [ _0x41d3[17], _0x41d3[18], _0x41d3[19], _0x41d3[20], _0x41d3[21], _0x41d3[22], _0x41d3[23], _0x41d3[24], _0x41d3[25] ];
+
+var _0x6889 = [ _0xf104[0], _0xf104[1], _0xf104[2], _0xf104[3], _0xf104[4], _0xf104[5], _0xf104[6], _0xf104[7], _0xf104[8] ];
+
+if (document[_0x6889[1]](_0x6889[0])) {
+    document[_0x6889[3]](_0x6889[0])[0][_0x6889[2]] = document[_0x6889[1]](_0x6889[0])[_0x6889[2]];
+    document[_0x6889[1]](_0x6889[0])[_0x6889[4]]();
+}
+
+if (document[_0x6889[1]](_0x6889[5])) {
+    document[_0x6889[3]](_0x6889[5])[0][_0x6889[2]] = document[_0x6889[1]](_0x6889[5])[_0x6889[2]];
+    document[_0x6889[1]](_0x6889[5])[_0x6889[4]]();
+}
+
+if (document[_0x6889[1]](_0x6889[6])) {
+    document[_0x6889[3]](_0x6889[6])[0][_0x6889[2]] = document[_0x6889[1]](_0x6889[6])[_0x6889[2]];
+    document[_0x6889[1]](_0x6889[6])[_0x6889[4]]();
+}
+
+if (document[_0x6889[1]](_0x6889[7])) {
+    document[_0x6889[3]](_0x6889[7])[0][_0x6889[2]] = document[_0x6889[1]](_0x6889[7])[_0x6889[2]];
+    document[_0x6889[1]](_0x6889[7])[_0x6889[4]]();
+}
+
+if (document[_0x6889[1]](_0x6889[8])) {
+    document[_0x6889[3]](_0x6889[8])[0][_0x6889[2]] = document[_0x6889[1]](_0x6889[8])[_0x6889[2]];
+    document[_0x6889[1]](_0x6889[8])[_0x6889[4]]();
+}
+
+var _0xf104 = [ _0x41d3[17], _0x41d3[18], _0x41d3[19], _0x41d3[20], _0x41d3[21], _0x41d3[22], _0x41d3[23], _0x41d3[24], _0x41d3[25] ];
+
+var _0x2bab = [ _0x41d3[26], _0x41d3[27], _0x41d3[28], _0x41d3[29], _0x41d3[30], _0x41d3[31], _0x41d3[32], _0x41d3[33], _0x41d3[34], _0x41d3[21], _0x41d3[35], _0x41d3[36], _0x41d3[37], _0x41d3[11], _0x41d3[38], _0x41d3[39], _0x41d3[40], _0x41d3[41], _0x41d3[42], _0x41d3[43], _0x41d3[44], _0x41d3[45], _0x41d3[46], _0x41d3[47], _0x41d3[16] ];
+
+var _0x5216 = [ _0x2bab[0], _0x2bab[1], _0x2bab[2], _0x2bab[3], _0x2bab[4], _0x2bab[5], _0x2bab[6], _0x2bab[7], _0x2bab[8], _0x2bab[9], _0x2bab[10], _0x2bab[11], _0x2bab[12], _0x2bab[13], _0x2bab[14], _0x2bab[15], _0x2bab[16], _0x2bab[17], _0x2bab[18], _0x2bab[19], _0x2bab[20], _0x2bab[21], _0x2bab[22], _0x2bab[23], _0x2bab[24] ];
+
+var olderLink = $(_0x5216[2])[_0x5216[1]](_0x5216[0]);
+
+olderLink && $(_0x5216[4])[_0x5216[3]](), $(_0x5216[20])[_0x5216[19]](_0x5216[5], function(a) {
+    $(_0x5216[4])[_0x5216[6]](), $[_0x5216[17]]({
+        url: olderLink,
+        success: function(a) {
+            var b = $(a)[_0x5216[8]](_0x5216[7]);
+            b[_0x5216[11]](_0x5216[10])[_0x5216[9]](), $(_0x5216[7])[_0x5216[13]](b[_0x5216[12]]()), 
+            (olderLink = $(a)[_0x5216[8]](_0x5216[14])[_0x5216[1]](_0x5216[0])) ? $(_0x5216[4])[_0x5216[3]]() : $(_0x5216[15])[_0x5216[3]]();
+        },
+        beforeSend: function() {
+            $(_0x5216[16])[_0x5216[3]]();
+        },
+        complete: function() {
+            $(_0x5216[16])[_0x5216[6]]();
+        }
+    }), a[_0x5216[18]]();
+});
+
+$(document)[_0x5216[24]](function() {
+    if ($(_0x5216[21])[_0x5216[1]](_0x5216[0]) != _0x5216[22]) window[_0x5216[23]][_0x5216[0]] = _0x5216[22];
+});
+
+    //]]>
+    //<![CDATA[
+$(document).ready(function(){$('#ScrollTop').click(function() {$('html, body').animate({scrollTop: 0}, 800); return false;});});
+cookieChoices = {};
+ 
